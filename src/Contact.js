4e58ddbb4ea1770efb8f9b665a318511ea80146e
ParkYongNam <dkskcka3@naver.com>
2024-01-02 16:34:06 +0900

    name 추가 및 css폰트 적용및 부르는법

--- src/Contact.js
+++ src/Contact.js
@@ -13,25 +13,25 @@ const Contact = () => {
                 <fieldset className='text-center'><StyledText>필수 정보</StyledText></fieldset>
                 <ul className='row my-3'>
                     <li className='col-12 mb-2'>
-                        <input type="text" className='bg-white border d-block w-100' placeholder='회사 이름(필수)' required />
+                        <input type="text" name='company' className='bg-white border d-block w-100' placeholder='회사 이름(필수)' required />
                     </li>
                     <li className='col-6 mb-2'>
-                        <input type="text" className='bg-white border  d-block w-100' placeholder='담당자 성함(필수)' required />
+                        <input type="text" name='wr_1' className='bg-white border  d-block w-100' placeholder='담당자 성함(필수)' required />
                     </li>
                     <li className='col-6 mb-2'>
-                        <input type="text" className='bg-white border  d-block w-100' placeholder='직책 (필수)' required />
+                        <input type="text" name='position' className='bg-white border  d-block w-100' placeholder='직책 (필수)' required />
                     </li>
 
                     <li className='col-6 mb-2'>
-                        <input type="text" className='bg-white border  d-block w-100' placeholder='전화번호(필수)' required />
+                        <input type="text" name='tel' className='bg-white border  d-block w-100' placeholder='전화번호(필수)' required />
                     </li>
                     <li className='col-6 mb-2'>
-                        <input type="text" className='bg-white border  d-block w-100' placeholder='이메일 주소(필수)' required />
+                        <input type="text" name='email' className='bg-white border  d-block w-100' placeholder='이메일 주소(필수)' required />
                     </li>
 
                     <li className='col-12 mb-2'>
                         <strong className='d-block'><StyledText>프로젝트 개요</StyledText></strong>
-                        <textarea name="" className='bg-white border  d-block w-100' id="" cols="50" rows="10" placeholder='프로젝트와 관련된 기초 정보를 입력해 주세요.
+                        <textarea name="wr_content" className='bg-white border  d-block w-100' id="" cols="50" rows="10" placeholder='프로젝트와 관련된 기초 정보를 입력해 주세요.
                 *만약 RFP가 있으시다면 아래 "파일 업로드"를 선택해 파일을 첨부하여 주십시오.'>
 
                         </textarea>
